8368941554e3f60d7373759863265550dd9cf34b
wangyibu <wangyibu@gitlab.gridsum.com>
2016-08-31 23:34:42 +0800

    11

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,23 @@ var gulp = require('gulp'),
 var sass = require('gulp-sass');
 
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     gulp.src('style/**/*.sass')
         .pipe(sass())
-        .pipe(gulp.dest(function (f) {
+        .pipe(gulp.dest(function(f) {
             return f.base;
         }))
 });
 
 
-gulp.task('develop-ts_single_compile', function () {
-    return gulp.src(['**/*.ts', '!node_modules/**/*.ts', '!typings/**/*.ts'])
+gulp.task('develop-ts_single_compile', function() {
+    return gulp.src(['**/*.ts', '!node_modules/**/*.ts'])
         .pipe(ts(tsProject))
         .pipe(gulp.dest("."));
 });
 
 
-gulp.task('default', ['sass'], function () {
+gulp.task('default', ['sass'], function() {
     gulp.watch('style/**/*.sass', ['sass']);
     gulp.watch('**/*.ts', ['develop-ts_single_compile']);
 })
-
-
