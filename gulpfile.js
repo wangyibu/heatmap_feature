9a472ad45c16359cf0cb094ec914a6b75fde431e
wangyibu <wangyibu@gitlab.gridsum.com>
2016-08-31 22:20:56 +0800

    11

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 // Sass configuration
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    watch = require("gulp-watch"),
+    ts = require('gulp-typescript'),
+    changed = require('gulp-changed'),
+    tsProject = ts.createProject('tsconfig.json');
 var sass = require('gulp-sass');
 
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     gulp.src('style/**/*.sass')
         .pipe(sass())
-        .pipe(gulp.dest(function(f) {
+        .pipe(gulp.dest(function (f) {
             return f.base;
         }))
 });
 
-gulp.task('default', ['sass'], function() {
+
+gulp.task('develop-ts_single_compile', function () {
+    return gulp.src(['**/*.ts', '!node_modules/**/*.ts', '!typings/**/*.ts'])
+        .pipe(ts(tsProject))
+        .pipe(gulp.dest("."));
+});
+
+
+gulp.task('default', ['sass'], function () {
     gulp.watch('style/**/*.sass', ['sass']);
+    gulp.watch('**/*.ts', ['develop-ts_single_compile']);
 })
+
+
